e59e809d2ec34a6c66a209e087cbac94443bf69d
Walter Restifo <walterrestifo93@gmail.com>
2022-11-24 13:18:02 +0100

    complete task 1

--- pokeshop/src/App.js
+++ pokeshop/src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Header from "./components/Header";
+import ShoppingItems from "./components/ShoppingItems";
 
 function App() {
-  return;
+  return (
+    <div className="grid-container">
+      <Header />
+      <ShoppingItems />
+    </div>
+  );
 }
 
 export default App;
