d8206fb315c5b2ec35aef42b860cade451ceade5
Walter Restifo <116001970+WalterRestifo@users.noreply.github.com>
2022-11-24 16:34:45 +0100

    Feature/task3 (#3)
    * fix image of Item.js
    
    * complete task 3
    
    * improve the id with nanoid

--- pokeshop/src/components/Item.js
+++ pokeshop/src/components/Item.js
@@ -1,34 +1,42 @@
 import { useEffect, useState } from "react";
+import { nanoid } from "nanoid";
 
-export default function Item({ keyValue, url }) {
+export default function Item({ keyValue, url, onAddToCart, isAddable }) {
   const [infoAboutItem, setInfoAboutItem] = useState({
     image: "",
     cost: 0,
     name: "",
+    url: "",
   });
-
   useEffect(() => {
     async function fetchInfoAboutItem() {
       try {
         const response = await fetch(url);
         const data = await response.json();
-        console.log("data: ", data);
         setInfoAboutItem({
-          image: "",
+          image: data.sprites.default,
           cost: data.cost,
           name: data.name,
+          url: url,
+          id: nanoid(),
         });
+        console.log(data);
       } catch (error) {
         console.error("error by fetching in Items.js", url);
       }
     }
     fetchInfoAboutItem();
   }, []);
-  console.log("infoAboutItem: ", infoAboutItem);
   return (
     <article className="shoppingItem__article" key={keyValue}>
+      <img src={infoAboutItem.image} alt={infoAboutItem.name} />
       <p>{infoAboutItem.name}</p>
       <p>{infoAboutItem.cost}$</p>
+      {isAddable && (
+        <button type="button" onClick={() => onAddToCart(infoAboutItem)}>
+          Add Item
+        </button>
+      )}
     </article>
   );
 }
